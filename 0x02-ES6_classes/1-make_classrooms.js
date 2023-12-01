d4bdf925f74cd4b324b6523c85b1a082faff226a
agun36 <aguntoroti10@gmail.com>
2023-12-01 13:12:32 +0000

    eslint-disable

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,6 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
+  /* eslint-disable */
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
