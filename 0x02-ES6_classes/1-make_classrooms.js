f32946f12d57015a8c71f50de9ccfc97195cfb38
agun36 <aguntoroti10@gmail.com>
2023-12-01 12:55:45 +0000

    fixing the bugs

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,20 @@
 import ClassRoom from './0-classroom.js';
 
-export default function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+// export default function initializeRooms() {
+//   return [
+//     new ClassRoom(19),
+//     new ClassRoom(20),
+//     new ClassRoom(34),
+//   ];
+// }
+class initializeRooms {
+  constructor() {
+    return [
+      new ClassRoom(19),
+      new ClassRoom(20),
+      new ClassRoom(34),
+    ];
+  }
 }
+
+export default initializeRooms;
