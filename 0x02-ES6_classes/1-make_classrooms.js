f7e7f238946ead05a4e9682fe910969d20e08343
agun36 <aguntoroti10@gmail.com>
2023-12-01 12:46:45 +0000

    1-make_room

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,6 @@
 import ClassRoom from './0-classroom.js';
 
-const initializeRooms = () => {
+export default function initializeRooms() {
   const sizes = [19, 20, 34];
   return sizes.map(size => new ClassRoom(size));
 }
-
-export default initializeRooms;
