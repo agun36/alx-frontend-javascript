e282a1af406d85a8d1e6cbe660e60b8c231dcafe
agun36 <aguntoroti10@gmail.com>
2023-12-01 13:45:34 +0000

    eslint fix bugs

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,29 +1,15 @@
-// 10-car.js
-
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  get brand() {
-    return this[_brand];
-  }
-
-  get motor() {
-    return this[_motor];
-  }
-
-  get color() {
-    return this[_color];
+  static get [Symbol.species]() {
+    return this.prototype.constructor;
   }
 
   cloneCar() {
-    return new this.constructor();
+    return new (this.constructor[Symbol.species])();
   }
 }
--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,24 +1,12 @@
-// 100-evcar.js
-
-import Car from './10-car.js';
-
-const _range = Symbol('range');
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this[_range] = range;
+    this._range = range;
   }
 
-  get range() {
-    return this[_range];
-  }
-
-  cloneCar() {
-    const carClone = new Car();
-    Object.getOwnPropertyNames(this).forEach((prop) => {
-      carClone[prop] = undefined;
-    });
-    return carClone;
+  static get [Symbol.species]() {
+    return Car;
   }
 }
