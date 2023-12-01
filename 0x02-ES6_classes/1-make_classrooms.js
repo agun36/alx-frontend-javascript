27bf2678d16bb1f30e65cc52a03a063402468c83
agun36 <aguntoroti10@gmail.com>
2023-12-01 13:01:31 +0000

    changing the class to export function

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
-class initializeRooms {
-  constructor() {
-    return [
-      new ClassRoom(19),
-      new ClassRoom(20),
-      new ClassRoom(34),
-    ];
-  }
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
-
-export default initializeRooms;
