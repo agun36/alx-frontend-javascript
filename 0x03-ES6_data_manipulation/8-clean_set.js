0011e41e105f7b1162e58fd1569e3db9e82049ff
agun36 <aguntoroti10@gmail.com>
2023-12-04 20:07:36 +0000

    I just finish with all my tasks

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+const cleanSet = (set, startString) => {
+  let result = '';
+
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += `${value.slice(startString.length)}-`;
+    }
+  });
+
+  return result.slice(0, -1);
+};
+
+export default cleanSet;
