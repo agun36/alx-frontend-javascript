18b9febb78954d57e7cea35dea0077042f869d18
agun36 <aguntoroti10@gmail.com>
2023-12-04 20:12:17 +0000

    8 clean

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 const cleanSet = (set, startString) => {
-  let result = '';
-
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      result += `${value.slice(startString.length)}-`;
-    }
-  });
-
-  return result.slice(0, -1);
-};
-
-export default cleanSet;
+    const strings = [];
+  
+    if (startString === '' || typeof startString !== 'string') return '';
+    set.forEach((s) => {
+      if (typeof s === 'string' && s.startsWith(startString)) {
+        strings.push(s.slice(startString.length));
+      }
+    });
+    return strings.join('-');
+  };
+  
+  export default cleanSet;
