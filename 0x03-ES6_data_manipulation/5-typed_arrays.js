45816a05eab1c94a93c16fc36119a3d92ca7825f
agun36 <aguntoroti10@gmail.com>
2023-12-04 20:21:21 +0000

    main 5-typed_arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,9 @@
 const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
-
-  if (position < 0 || position >= view.length) {
-    throw new Error('Position outside range');
-  }
-
+  if (position > int8Array.length) throw new Error('Position outside range');
   view[position] = value;
-
-  return buffer;
+  return new DataView(buffer);
 };
 
 export default createInt8TypedArray;
